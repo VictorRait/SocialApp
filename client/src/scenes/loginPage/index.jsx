492d8b623785385a17395e2f2925e9dfb2200fba
VictorRait <victorlemuelrait@gmail.com>
2023-11-28 15:05:52 +0800

    completed navbar

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,31 @@
+import {useTheme} from "@emotion/react";
+import {Typography, useMediaQuery} from "@mui/material";
+import {Box} from "@mui/system";
+import Form from "./Form";
 function LoginPage() {
-	return <div>LoginPage</div>;
+	const theme = useTheme();
+
+	const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+
+	return (
+		<Box>
+			<Box width="100%" backgroundColor={theme.palette.background.alt}>
+				<Typography fontWeight="bold" fontSize="32px" color="primary">
+					Sociopedia
+				</Typography>
+			</Box>
+			<Box
+				width={isNonMobileScreens ? "50%" : "93%"}
+				p="2rem"
+				m="2rem auto"
+				borderRadius="1.5rem"
+				backgroundColor={theme.palette.background.alt}>
+				<Typography fontWeight="500" variant="h5" sx={{mb: "1.5rem"}}>
+					Welcome to Sociopedia, the Social Media for Sociopaths!
+				</Typography>
+			</Box>
+		</Box>
+	);
 }
 
 export default LoginPage;
