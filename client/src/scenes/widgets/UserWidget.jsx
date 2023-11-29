45e2939e6e70a4e1d50ac2a3de9c1cdd435d0870
VictorRait <victorlemuelrait@gmail.com>
2023-11-29 13:59:52 +0800

    added widgetwrapper and userimagewidget components

---
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -0,0 +1,8 @@
+import {
+	ManageAccountsOutlined,
+	EditOutline,
+	LocationOnOutline,
+	WorkOutlineOutlined,
+} from "@mui/icons-material";
+import {Box, Typography, Divider, useTheme} from "@mui/material";
+import UserImage from "components/UserImageWidget";
